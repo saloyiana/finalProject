2a451637be93b3dc4add39529c6514b6d610dc7d
sara <saronah1099@gmail.com>
2020-10-29 11:15:20 +0300

    edited runner.sh

--- test/e2e-js-test/runner.sh
+++ test/e2e-js-test/runner.sh
@@ -6,12 +6,12 @@ files=$testfile
 
 if [ -z $testfile ]
 then
-  files=$(ls test/e2e/*_test.js)
+  files=$(ls test/e2e-js-test/*_test.js)
 fi
 
 for test in $files
 do
-  node test/e2e/test_helper.js
+  node test/e2e-js-test/test_helper.js
   casperjs test $test
   ret=$?
   if [ ! $ret == "0" ]; then code=1; fi
