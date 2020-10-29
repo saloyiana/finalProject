39cb3f4a092e2f2a54e6f6eb48b9f3da707bd63b
sara <saronah1099@gmail.com>
2020-10-29 14:30:58 +0300

    removed port

--- test/e2e-js-test/homepage_test.js
+++ test/e2e-js-test/homepage_test.js
@@ -4,7 +4,7 @@
   require("./config")
 
   casper.test.begin('Home page looks sexy', 4, function suite(test) {
-    casper.start("http://front-end:8080/", function() {
+    casper.start("http://front-end/", function() {
       test.assertTitle("WeaveSocks", "homepage title is the one expected");
       test.assertTextExists("Login", "login link is present");
       test.assertNotVisible("ul.menu li.howdy", "user is not logged in");
