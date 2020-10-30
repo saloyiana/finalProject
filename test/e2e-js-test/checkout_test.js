458ab306cc1034d4d62885163d7923cdd9c6a7c2
sara <saronah1099@gmail.com>
2020-10-30 17:02:17 +0300

    8079

--- test/e2e-js-test/checkout_test.js
+++ test/e2e-js-test/checkout_test.js
@@ -6,7 +6,7 @@
 
   casper.test.begin("User buys some socks", 5, function(test) {
     // initial load and login
-    casper.start("http://front-end/", function() {
+    casper.start("http://front-end:8079/", function() {
       this.clickLabel("Login");
       this.fill("#login-modal form", {
         "username": "Eve_Berger",
--- test/e2e-js-test/login_test.js
+++ test/e2e-js-test/login_test.js
@@ -4,7 +4,7 @@
   require("./config");
 
   casper.test.begin("User logs in", 3, function suite(test) {
-    casper.start("http://front-end/", function() {
+    casper.start("http://front-end:8079/", function() {
       test.assertNotVisible("#login-modal", "user does not see the login dialogue");
 
       this.clickLabel("Login");
